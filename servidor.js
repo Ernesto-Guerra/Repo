bb717277051373313292e653ba8b838cf8b018e7
Ernesto Guerra <el_guerr@hotmail.com>
2019-01-26 14:03:25 -0600

    first

---
+++ servidor.js
@@ -0,0 +1,48 @@
+//"use stric"
+var express = require('express')
+var aplicacion = express()
+const os = require('os');
+
+var interface = os.networkInterfaces();
+var ipDinamic;
+for(var k in interface){
+    for(var k2 in interface[k]){
+        var address = interface[k][k2]
+        if(address.family == 'IPv4' && !address.internal ){
+            ipDinamic = address.address.toString();
+            console.log(ipDinamic);
+        }
+    }
+}
+
+const net = require('net');
+const server = require('http').Server(aplicacion)
+const socket = require('socket.io')(server)
+const {StringDecoder} = require('string_decoder')
+// const decoder = new  StringDecoder('utf8')
+
+var HOST = ipDinamic;
+
+
+var PORT = process.env.PORT || 5000
+
+
+var ser = net.createServer(function(so){    
+    
+    console.log('Usuario Conectado' + so.remoteAddress + ':' + so.remotePort)
+
+     so.on('connect', function(){
+         console.log('Usuario Nuevo')
+     })
+    
+    so.on('data', function(data){        
+        console.log(data.toString())
+        so.write('FELICIDADES CAMPEON\n')
+    })
+
+    so.on('close',function(){
+        console.log('Usuario Desconectado')
+    })
+})
+
+ser.listen(PORT, HOST);
